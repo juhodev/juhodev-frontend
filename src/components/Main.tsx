a36faa359acde5f9a88b37d30437d04b71fa3123
Juhodev <jooo.dev@gmail.com>
2020-10-26 09:11:08 +0200

    Add navigation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Auth from './Auth';
 import Home from './Home';
 import Landing from './Landing';
+import Navigation from './Navigation';
 
 const Main = () => {
 	return (
 		<Router>
+			<Navigation />
 			<Route exact path="/" component={Landing} />
 			<Route path="/home" component={Home} />
 			<Route path="/auth" component={Auth} />
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+const { useState } = React;
+
+type Path = {
+	to: string;
+	name: string;
+};
+
+const Navigation = () => {
+	const [currentTab, setCurrentTab] = useState(window.location.pathname);
+
+	const paths: Path[] = [
+		{ to: '/home', name: 'Home' },
+		{ to: '/images', name: 'Images' },
+	];
+
+	const links: JSX.Element[] = paths.map(
+		(path): JSX.Element => {
+			let className = 'mx-5 text-3xl text-gray-200';
+
+			if (currentTab === path.to) {
+				className += ' font-bold border-b-2 border-blue-600';
+			}
+
+			return (
+				<Link
+					key={path.name}
+					className={className}
+					to={path.to}
+					onClick={() => setCurrentTab(path.to)}
+				>
+					{path.name}
+				</Link>
+			);
+		},
+	);
+
+	return <div className="pl-12 pt-2 pb-4 mx-1/5">{links}</div>;
+};
+
+export default Navigation;
