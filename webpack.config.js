87fd171f99e2217e61d10312f3645d80dc974b61
Juhodev <jooo.dev@gmail.com>
2020-11-29 17:40:18 +0200

    Add code splitting
    Right now this doesn't really do anything. When building for production
    the bundle is split in to three files runtime, vendors and main.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.tsx',
 	output: {
-		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].js',
+		publicPath: 'dist',
 	},
 	resolve: {
 		extensions: ['.ts', '.tsx', '.js', '.json'],
 	},
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({ title: 'Baavo', template: 'index.html' }),
+	],
 	module: {
 		rules: [
 			{
