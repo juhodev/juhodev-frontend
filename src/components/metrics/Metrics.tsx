6089f838bf6f58f1c8e3a2220a373922e1244f22
Juhodev <jooo.dev@gmail.com>
2021-01-24 14:06:31 +0200

    Display cache logs better in metrics

--- src/components/metrics/Metrics.tsx
+++ src/components/metrics/Metrics.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { fetchDemoWorkerStatus, fetchMetrics } from '../../api/api';
 import { SiteMetric } from '../../api/types';
 import { formatSeconds, millisecondsToSeconds } from '../../ts/timeUtils';
+import PieChart from '../utils/PieChart';
 import MetricGraph from './MetricGraph';
+import MetricPie from './MetricPie';
 
 const { useEffect, useState } = React;
 
@@ -41,16 +43,45 @@ const Metrics = () => {
 		setMetrics(metrics);
 	};
 
-	const graphs: React.ReactNode[] = metrics.map((metric, i) => (
-		<MetricGraph
+	// If the metric doesn't start with `cache` then it'll be a line chart
+	const lineCharts: React.ReactNode[] = metrics
+		.filter((metric) => !metric.name.startsWith('cache'))
+		.map((metric, i) => (
+			<MetricGraph
+				key={i}
+				name={metric.name}
+				data={metric.values}
+				dataFormat={(data) => `${Math.round(data)}ms`}
+			/>
+		));
+
+	const cacheData: SiteMetric[] = metrics.filter((metric) =>
+		metric.name.startsWith('cache'),
+	);
+	console.log(cacheData);
+	const pieCharts: React.ReactNode[] = cacheData.map((data, i) => (
+		<MetricPie
 			key={i}
-			name={metric.name}
-			data={metric.values}
-			dataFormat={(data) => `${Math.round(data)}ms`}
+			name={data.name}
+			total={data.values.length.toString()}
+			data={[
+				{
+					name: 'Cache hits',
+					num: data.values.filter((x) => x === 1).length,
+				},
+				{
+					name: 'Cache misses',
+					num: data.values.filter((x) => x === 0).length,
+				},
+			]}
 		/>
 	));
 
-	return <div className="grid grid-cols-3 gap-4">{graphs}</div>;
+	return (
+		<div className="grid grid-cols-3 gap-4">
+			{[...lineCharts, ...pieCharts]}
+		</div>
+	);
 };
 
 export default Metrics;
